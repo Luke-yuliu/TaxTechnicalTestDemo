e374c3fbbe6ade5be40b6dfb1c5068258963e468
Suranjit Saha <69782517+SuranjitSaha@users.noreply.github.com>
2020-09-01 16:29:39 +1200

    Add files via upload

---
+++ taxTechinicalTestCases/src/test/java/TaxTechnicalTestCases.java
@@ -0,0 +1,64 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TaxTechnicalTestCases {
+
+    private WebDriver driver;
+    private String url = "https://www.taxtechnical.ird.govt.nz/";
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    public void externalLinkIRMain() throws InterruptedException {
+        WebElement element = driver.findElement(By.xpath("/html//div[@id='header']/div[@class='row']/div//ul[@class='tp-text-links']//a[@title='IR main site']"));
+        element.click();
+    }
+
+    @Test
+    public void externalLinkTP() {
+        WebElement element = driver.findElement(By.xpath("/html//div[@id='header']/div[@class='row']/div//ul[@class='tp-text-links']//a[@title='IR Tax Policy']"));
+        element.click();
+    }
+
+    @Test
+    public void testBreadcrumb() {
+        WebElement element = driver.findElement(By.xpath("/html//div[@id='header']//div[@role='menu']/div/div[3]/a[@href='/consultations']"));
+        element.click();
+        WebElement breadCrumbLocator = driver.findElement(By.xpath("/html//div[@id='content']/div[@class='row']/div[1]//a[@title='Home']"));
+        breadCrumbLocator.click();
+        Assert.assertEquals("Check breadcrumb","https://www.taxtechnical.ird.govt.nz/",driver.getCurrentUrl());
+    }
+
+    @Test
+    public void searchBestMatchResult() {
+        WebElement searchField = driver.findElement(By.xpath("//form[@id='search-form']//input[@name='tt-search-form__input']"));
+        WebElement searchButton = driver.findElement(By.xpath("//form[@id='search-form']//button[@type='submit']"));
+        searchField.sendKeys("My Tax");
+        searchButton.click();
+        WebElement buttonField = driver.findElement(By.xpath("//div[@id=\"content\"]/div/section/div/div[1]/div[2]/div/button"));
+        Assert.assertEquals("Search displays best match","Best match",buttonField.getText());
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(4000);
+        driver.quit();
+        
+    }
+}
